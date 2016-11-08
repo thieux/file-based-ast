40f12f3b079ef59525ff8bf72cd6347ed5d5405b
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-11-08 22:57:14 +0100

    Make expected & actual more obvious in assertEquals message

--- interpret.bash
+++ interpret.bash
@@ -4,7 +4,7 @@ function assertEquals() {
   if [[ $1 = $2 ]]; then
     echo "test $1 = $2"
   else 
-    echo "FAILURE: expected=$1; actual=$2"
+    echo "FAILURE: expected='$1'; actual='$2'"
     exit 1
   fi
 }
