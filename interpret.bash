4943b0f76fb024b6bddf0dfccf69da47b57cac68
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-11-08 22:54:06 +0100

    Make assertEquals to fail fast

--- interpret.bash
+++ interpret.bash
@@ -5,9 +5,11 @@ function assertEquals() {
     echo "test $1 = $2"
   else 
     echo "FAILURE: expected=$1; actual=$2"
+    exit 1
   fi
 }
 
 assertEquals 1 1
 assertEquals 1 2
+assertEquals 1 3
 
